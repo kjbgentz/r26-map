bb68d178c868124ec2e548a07004344937c21d9a
kjbgentz <49692649+kjbgentz@users.noreply.github.com>
2026-07-19 08:41:30 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'r26-map-v9';
-const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './pdf/bike_maps.pdf', './pdf/stay_02.pdf', './pdf/stay_03.pdf', './pdf/stay_04.pdf', './pdf/stay_05.pdf', './pdf/stay_06.pdf', './pdf/stay_07.pdf', './pdf/town_02.pdf', './pdf/town_03.pdf', './pdf/town_04.pdf', './pdf/town_05.pdf', './pdf/town_06.pdf', './pdf/town_07.pdf', './pdf/vehicle_routes.pdf', './pdf/vendors_02.pdf', './pdf/vendors_03.pdf', './pdf/vendors_04.pdf', './pdf/vendors_05.pdf', './pdf/vendors_06.pdf', './pdf/vendors_07.pdf'];
+const CACHE = 'r26-map-v12';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './lib/pdf.min.js', './lib/pdf.worker.min.js', './pdf/bike_maps.pdf', './pdf/stay_02.pdf', './pdf/stay_03.pdf', './pdf/stay_04.pdf', './pdf/stay_05.pdf', './pdf/stay_06.pdf', './pdf/stay_07.pdf', './pdf/town_02.pdf', './pdf/town_03.pdf', './pdf/town_04.pdf', './pdf/town_05.pdf', './pdf/town_06.pdf', './pdf/town_07.pdf', './pdf/vehicle_routes.pdf', './pdf/vendors_02.pdf', './pdf/vendors_03.pdf', './pdf/vendors_04.pdf', './pdf/vendors_05.pdf', './pdf/vendors_06.pdf', './pdf/vendors_07.pdf'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
